73484c80793f8ebeea3d22536dd3c67a3db64200
tawhidul2122 <tawhidul2122@gmail.com>
2022-09-22 01:46:05 +0600

    Arraylist .add --> new element at the end, O(n)

---
+++ 412 FizzBuzz.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        
+        List<String> newArray= new ArrayList<> (n);
+        
+        for(int i=1;i<=n;i++){
+            boolean by3= i%3==0;
+            boolean by5= i%5==0;
+            if( (by3) && (by5)){
+                newArray.add("FizzBuzz");
+                
+            }else if(by5){
+                newArray.add("Buzz");
+            }else if(by3){
+                newArray.add("Fizz");
+            }else newArray.add(String.valueOf(i));
+        }
+        
+        return newArray;
+        
+    }
+}
