682a69b2daaccb6147c5c33803d84637e2ea9da3
Towhidul Islam <tawhidul2122@gmail.com>
2022-10-19 14:06:24 +0600

    Create 1295 Find Numbers with Even Number of Digits.cpp

---
+++ 1295 Find Numbers with Even Number of Digits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    
+    int findNumbers(vector<int>& nums) {
+        int digit,ans=0;
+        for (int num: nums){
+            digit=0;
+            while(num){
+                num=num/10;
+                digit++;
+            }
+            if(digit%2==0){
+                ans++;
+            }
+            
+        }
+        return ans;
+    }
+};
