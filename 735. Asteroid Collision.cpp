15dd3207b7f14705529fb44e2e90fc63694f3820
Towhidul Islam <tawhidul2122@gmail.com>
2023-07-20 14:11:40 +0600

    Create 735. Asteroid Collision.cpp

---
+++ 735. Asteroid Collision.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int> stk;
+        
+        for(int& cur : asteroids){
+            bool exploded = false;
+            while(!stk.empty() && stk.top() > 0 && cur < 0){
+                int prev = stk.top();
+                if(abs(prev) > abs(cur)){
+                    //do nothing
+                    exploded = true;
+                    break;
+                }else if(abs(prev) == abs(cur)){
+                    stk.pop();
+                    exploded = true;
+                    break;
+                }else{
+                    //abs(prev) < abs(cur)
+                    stk.pop();
+                    //cur keeps going left
+                }
+            }
+
+            if(!exploded){
+                stk.push(cur);
+            }
+        }
+
+        asteroids.clear();
+
+        while(!stk.empty()){
+            asteroids.insert(asteroids.begin(), stk.top());
+            stk.pop();
+        }
+        
+        return asteroids;
+    }
+};
